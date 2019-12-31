a1b561615501ddf0a14e9691a8cfb11dde3f42b8
Frank Wan <frankwan27@gmail.com>
2019-12-31 23:14:05 +0530

    Added a pulsing effect for notes

---
+++ Assets/Scripts/Pulse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulse : MonoBehaviour
+{
+    public float pulseFactor = 1.1f;
+    Vector3 originalSize;
+    Conductor conductor;
+    int currentBeat = -20;
+
+    void Start()
+    {
+        conductor = GetComponent<NoteContoller>().conductor;
+        originalSize = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float smoothSpeed = 0.1f;
+        transform.localScale = Vector3.Lerp(transform.localScale, originalSize, smoothSpeed);
+
+        if((int)conductor.songPositionInBeats - currentBeat >= 1)
+        {
+            currentBeat = (int)conductor.songPositionInBeats;
+            transform.localScale = originalSize * pulseFactor;
+        }
+    }
+}
